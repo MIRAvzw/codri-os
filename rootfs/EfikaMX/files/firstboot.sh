165f7a4009102e2a3c567607c5c441ffc8a49d37
Tim Besard <tim.besard@gmail.com>
2012-04-27 11:28:44 +0200

    RootFS bugfixes.

--- rootfs/EfikaMX/files/firstboot.sh
+++ rootfs/EfikaMX/files/firstboot.sh
@@ -8,6 +8,7 @@ echo $HOSTNAME > /etc/hostname
 echo "127.0.0.1       localhost.localdomain localhost" > /etc/hosts
 echo "127.0.1.1       $HOSTNAME.$DOMAIN $HOSTNAME" >> /etc/hosts
 invoke-rc.d hostname.sh start
+invoke-rc.d avahi-daemon restart
 
 # Configure all remaining packages
 dpkg --configure -a
